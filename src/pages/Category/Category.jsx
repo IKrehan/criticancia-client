8f2a7d80abc1a57ce89afdb95681523e612551ab
Irvig Krehan <irvigkrehan@hotmail.com>
2021-05-09 14:04:51 -0300

    add Loading state to categories pages

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,13 +1,14 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import axios from "axios";
 
 import NavBar from "../../components/NavBar/Navbar";
 import Shape from "../../assets/imgs/shape.svg";
 import { useState, useEffect } from "react";
 import Feed from "../../components/Feed/Feed";
+import Loading from "../../components/Loading/Loading";
 
 function Category(props) {
-  const [isLoaded, setiILoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -18,49 +19,42 @@ function Category(props) {
       })
       .then((response) => {
         setPosts(response.data.data);
+        setIsLoaded(true);
       })
       .catch((error) => console.log(error));
-
-    setiILoaded(true);
   }, [props]);
 
-  if (isLoaded) {
-    return (
-      <div
-        className="mt-n3"
-        style={{
-          backgroundImage: `url(${Shape})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-        }}
-      >
-        <Container className="pt-4" fluid>
-          <NavBar category={props.category} />
-          <Feed posts={posts} />
-        </Container>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className="mt-n3"
-        style={{
-          backgroundImage: `url(${Shape})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-        }}
-      >
-        <Container className="pt-4" fluid>
-          <NavBar category={props.category} />
-          <Row className="m-5 justify-content-center">
-            <Col md={12} className="my-3 justify-content-center">
-              <h1 style={{ color: "white" }}>Loading...</h1>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+  return isLoaded ? (
+    <div
+      className="mt-n3"
+      style={{
+        backgroundImage: `url(${Shape})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+      }}
+    >
+      <Container className="pt-4" fluid>
+        <NavBar category={props.category} />
+        <Feed posts={posts} />
+      </Container>
+    </div>
+  ) : (
+    <div
+      className="mt-n3"
+      style={{
+        backgroundImage: `url(${Shape})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+      }}
+    >
+      <Container className="pt-4" fluid>
+        <NavBar category={props.category} />
+        <div className="load-box">
+          <Loading />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default Category;
